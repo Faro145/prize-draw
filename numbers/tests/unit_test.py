dc8ccabe7d87d47d9144c6fadd9faaaac399a66b
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-11-28 19:39:29 +0000

    Update unit_test.py

--- numbers/tests/unit_test.py
+++ numbers/tests/unit_test.py
@@ -8,10 +8,10 @@ class TestBase(TestCase):
         return app
 
 class TestNumbers(TestBase):
-
-def test_numbers(self):
-    number_one = "1"
-    number_two = "9"
-    number_three = "0"
-    response = self.client.get(url_for('numbers'))
-    self.assertIn(response.data, numbers)
+    
+    def test_numbers(self):
+        number_one = "1"
+        number_two = "9"
+        number_three = "0"
+        response = self.client.get(url_for('numbers'))
+        self.assertIn(response.data, numbers)
