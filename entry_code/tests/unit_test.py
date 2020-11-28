d51552d8803c046d8b8b76de3a2e30effc57b8eb
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-11-28 19:36:51 +0000

    Update unit_test.py

--- entry_code/tests/unit_test.py
+++ entry_code/tests/unit_test.py
@@ -8,20 +8,19 @@ class TestBase(TestCase):
         return app
 
 class TestEntryCode(TestBase):
-
-def test_entry_code(self):
-    letters = "BpT"
-    numbers = "538"
-    response = self.client.get(url_for('entry_code'))
-    self.assertIn(response.data, entry_code)
-
-def test_big_prize(self):
-    response = self.client.post(
+    def test_entry_code(self):
+        letters = "BpT"
+        numbers = "538"
+        response = self.client.get(url_for('entry_code'))
+        self.assertIn(response.data, entry_code)
+    
+    def test_big_prize(self):
+        response = self.client.post(
             url_for('prize_code'),
             data="Bzk371",
             follow_redirects=True
         )
-    self.assertIn(b'You won a big prize', response.data)
+        self.assertIn(b'You won a big prize', response.data)
 
 
 
