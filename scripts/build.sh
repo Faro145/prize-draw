2135dc859a020792f4241d168248f9988799a473
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-12-04 11:10:57 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 . ~/.bashrc
-pip3 install --user ansible
+pip install --user ansible
 ansible-playbook -i ./inventory ./playbook.yaml
