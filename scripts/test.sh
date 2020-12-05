393f83c22cef7ff76eef63406b65ae062c86c26a
Faro145 <rossfarq@gmail.com>
2020-12-05 23:04:38 +0000

    Update Pipeline scripts and add nginx

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-sudo apt install python3 python3-pip python3-venv -y
+sudo apt install python3 python3-pip python3-venv python3-flask -y
 
 python3 -m venv venv
 . venv/bin/activate
 
 pip3 install -r requirements.txt
 
-pytest --cov ./service1/application --cov-report term-missing
-pytest --cov ./service2/application --cov-report term-missing
-pytest --cov ./service3/application --cov-report term-missing
-pytest --cov ./service3/application --cov-report term-missing
+pytest --cov ./service_1/application --cov-report term-missing
+pytest --cov ./service_2/application --cov-report term-missing
+pytest --cov ./service_3/application --cov-report term-missing
+pytest --cov ./service_4/application --cov-report term-missing
 
 deactivate
 rm -rf venv
