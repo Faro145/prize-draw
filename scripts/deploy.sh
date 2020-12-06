af3352b7124279dfbe131200ae52d0896689fd8e
Faro145 <rossfarq@gmail.com>
2020-12-06 21:58:49 +0000

    gunicorn/nginx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+ssh swarm-manager << EOF
+git clone https://github.com/Faro145/prize-draw.git
+cd prize-draw
+docker pull rfarq75/service_1:latest
+docker pull rfarq75/service_2:latest
+docker pull rfarq75/service_3:latest
+docker pull rfarq75/service_4:latest
+docker pull nginx:alpine
 docker stack deploy --compose-file docker-compose.yaml prize-draw
-
+EOF
