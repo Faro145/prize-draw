d589f20c48d31a04ee994abb144336e1ab13dbdb
Faro145 <rossfarq@gmail.com>
2020-12-06 20:31:33 +0000

    gunicorn

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo docker build -f testing/Dockerfile -t prizedraw-testing-image .
-sudo docker run -it -d --name testing-container prizedraw-testing-image
+sudo docker run -e DB_URI=${DB_URI} -e KEY=${KEY}  -it -d --name testing-container prizedraw-testing-image
 
 sudo docker exec testing-container pytest ./service_1 --cov ./service_1
 sudo docker exec testing-container pytest ./service_2 --cov ./service_2
